d17923e2661d3911c3548d36aff37bf28bd8ca31
AnkDos <ankurpan96@gmail.com>
2019-02-14 02:43:58 +0000

    First routing done

--- app.rb
+++ app.rb
@@ -1,7 +1,16 @@
 require 'sinatra'
+require 'json'
 
-@json_ifsc = Hash.new
-@json_by_city = Hash.new
-@json_error_message = Hash.new
+json_ifsc = Hash.new
+json_by_city = Hash.new
+json_error_message = Hash.new
+@output = Hash.new
 ar = []
+
+json_error_message["errmsg"] = "Either Not Found OR Parameters Not stated Correctly"
+
+get '/' do
+      @output = json_error_message.to_json
+      erb:app
+end
 
